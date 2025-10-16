40499bc2cfa0d06d9b0736561a5d9860a13e0c97
christine-martin <cmartin1624@gmail.com>
2025-10-16 12:52:03 -0400

    Created new project,script
    Imported directory structure

---
+++ data/raw/citsci_species.r
@@ -0,0 +1,12 @@
+#EVR628 Project
+#Christine Martin
+#Citizen Science x Chondrichthyans
+#Species Processing, Analysis, and Visualizations
+
+#Loading packages
+library(EVR628tools)
+
+#Creating directories
+EVR628tools::create_dirs()
+
+
